ce213cb7df508845f7e17ca18e6e5335f7503dce
Asli Uyanik <au643@stern.nyu.edu>
2019-06-05 19:26:29 -0400

    generating computer choice

--- game.py
+++ game.py
@@ -1,3 +1,30 @@
 # game.py
+#you have to import a module 
+import random
 
 print("Rock, Paper, Scissors, Shoot!")
+
+#Capture inputs
+
+user_choice = input ("please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes)")
+
+print ("-------")
+print ("User Choice:", user_choice)
+#validate inputs
+if user_choice not in ["rock", "paper", "scissors"]:
+    print ("invalid selection, please try again...")
+    exit()
+
+#generate computer selection
+print ("generating")
+
+computer_choice = random.choice (["rock", "paper", "scissors"])
+
+print ("-------")
+print ("computer_choice:", computer_choice)
+
+#determine the winner
+
+
+#display final outputs / outcomes
+
