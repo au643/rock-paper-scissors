4fb11f5c4816ae2bf122e42acacaefc3a82151d5
au643 <51296607+au643@users.noreply.github.com>
2019-06-05 19:16:28 -0400

    Add files via upload

--- rock-paper-scissors/game.py
+++ rock-paper-scissors/game.py
@@ -1,3 +1,21 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+#Capture inputs
+
+user_choice = input ("please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes)")
+
+print ("-------")
+print ("User Choice:", user_choice)
+#validate inputs
+if user_choice not in ["rock", "paper", "scissors"]:
+    print ("invalid selection, please try again...")
+    exit()
+
+#generate computer selection
+
+#determine the winner
+
+#display final outputs / outcomes
+
